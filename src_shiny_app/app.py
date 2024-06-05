46f6311e49aca69c40393df3eae5475010e66832
Nicky Nicolson <nicky.nicolson@gmail.com>
2024-06-05 20:40:28 +0100

    Update app.py

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -1,7 +1,7 @@
 from shiny import ui, render, App
 import numpy as np
 import matplotlib.pyplot as plt
-
+import requests
 
 app_ui = ui.page_fluid(
     ui.layout_sidebar(
@@ -20,8 +20,10 @@ def server(input, output, session):
     @output
     @render.plot
     def histogram():
+        r = requests.get('https://api.gbif.org/v1/dataset/cd6e21c8-9e8a-493a-8a76-fbf7862069e5')
+        title = r.json['title']
         x = 100 + np.random.randn(500)
-        plt.title("Histogram", size=20)
+        plt.title(title, size=20)
         plt.hist(x=x, bins=input.slider(), color="grey", ec="black")
 
 
