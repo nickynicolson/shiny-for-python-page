36396716806f7ec8d289823d5f3bda5623317c64
Nicky Nicolson <nicky.nicolson@gmail.com>
2024-06-05 21:56:33 +0100

    Update app.py

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -1,19 +1,31 @@
-import matplotlib.pyplot as plt
+from shiny import ui, render, App
 import numpy as np
-from shiny.express import ui, input, render
+import matplotlib.pyplot as plt
 import pyodide.http
+app_ui = ui.page_fluid(
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.h2("Page Charts"),
+            ui.hr(),
+            ui.input_slider(id="slider", label="Number of bins:", min=5, max=25, value=15)
+        ),
+        ui.panel_main(
+            ui.output_plot(id="histogram")
+        )
+    )
+)
 
-with ui.sidebar():
-    ui.input_slider("n", "N", 0, 100, 20)
-
-@render.plot(alt="A histogram")
-async def histogram():
-    url = 'https://api.gbif.org/v1/dataset/cd6e21c8-9e8a-493a-8a76-fbf7862069e5'
-    response = await pyodide.http.pyfetch(url)
-    if response.status != 200:
-        raise Exception(f"Error fetching {url()}: {response.status}")
-    data = await response.json()        
-    title = data['title']    
-    x = 100 + np.random.randn(500)
-    plt.title(title, size=20)
-    plt.hist(x=x, bins=input.slider(), color="grey", ec="black")
+def server(input, output, session):
+    @output
+    @render.plot
+    async def histogram():
+        url = 'https://api.gbif.org/v1/dataset/cd6e21c8-9e8a-493a-8a76-fbf7862069e5'
+        response = await pyodide.http.pyfetch(url)
+        if response.status != 200:
+            raise Exception(f"Error fetching {url()}: {response.status}")
+        data = await response.json()        
+        title = data['title']
+        x = 100 + np.random.randn(500)
+        plt.title(title, size=20)
+        plt.hist(x=x, bins=input.slider(), color="grey", ec="black")
+app = App(ui=app_ui, server=server)
